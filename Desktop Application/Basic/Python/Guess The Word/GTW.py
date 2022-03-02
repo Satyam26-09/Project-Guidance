910a2ee627963c0745d370d7d254519fb49a95bb
Satyam26-09 <satyam.sharma.cse25@heritageit.edu.in>
2022-03-02 13:44:49 +0530

    casesensetive problem-Solved

--- Desktop Application/Basic/Python/Guess The Word/GTW.py
+++ Desktop Application/Basic/Python/Guess The Word/GTW.py
@@ -12,35 +12,35 @@ def game():
     score=0
     wrong=0
     ans1=input("QUESTION 1 : I MAKE TWO PEOPLE OUT OF ONE:")
-    if ans1=='mirror':
+    if ans1.lower()=='mirror':
         print("!!!!YOU ARE RIGHT!!!!!")
         score=score+1
     else:
         print(":( YOU ARE WRONG :(")
         wrong=wrong+1
     ans2=input("QUESTION 2 : I AM WHITE WHEN I AM DIRTY AND BLACK WHEN I AM CLEAN:")
-    if ans2=='black board':
+    if ans2.lower()=='black board':
         print("!!!!!!!!YOU ARE RIGHT!!!!!!!")
         score=score+1
     else:
         print(":( YOU ARE WRONG")
         wrong=wrong+1
     ans3 = input("QUESTION 3 : THE MORE I TAKE AWAY THE MORE I BECOME:")
-    if ans3 == 'hole':
+    if ans3.lower() == 'hole':
         print("!!!!!!!!YOU ARE RIGHT!!!!!!!")
         score = score + 1
     else:
         print(":( YOU ARE WRONG")
         wrong = wrong + 1
     ans4 = input("QUESTION 4 : I HAVE TWO HANDS BUT I CAN NOT SCRATCH MYSELF:")
-    if ans4 == 'clock':
+    if ans4.lower() == 'clock':
         print("!!!!!!!!YOU ARE RIGHT!!!!!!!")
         score = score + 1
     else:
         print(":( YOU ARE WRONG")
         wrong = wrong + 1
     ans5 = input("QUESTION 5 : AM TALL WHEN I AM YOUNG AND I AM SHORT WHEN I AM OLD:")
-    if ans5 == 'pencil':
+    if ans5.lower() == 'pencil':
         print("!!!!!!!!YOU ARE RIGHT!!!!!!!")
         score = score + 1
     else:
